70c527ea3109562b76207e280a9a25db48c98130
OP-OP <preecha.t@egat.co.th>
2021-03-21 13:31:52 +0700

    UpdateAngular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,12 +9,14 @@ import {HttpClient} from '@angular/common/http'
 export class AppComponent  implements OnInit {
   title = 'The Dating App';
   users: any;
+  count:number = 0;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() : void{
     this.getUsers();
   }
+  
   getUsers(){
     this.http.get('https://localhost:5001/api/users').subscribe(response => {
       this.users = response;
